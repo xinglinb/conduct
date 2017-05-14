413cd6018f4d2fd341c920f7d8e3bd513a465182
xinglinb <986294216@qq.com>
2017-05-14 12:31:40 +0800

    a

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+window.onload=function(){
+  
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+        loop: true,
+        onSlideChangeStart: function(swiper,even){
+            
+        },
+        onSlideChangeEnd: function(swiper){
+            
+        }
+    }) 
+}
