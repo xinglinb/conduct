92beba92c30b57e96079aa195fc55b481df43b32
xinglinb <986294216@qq.com>
2017-05-14 16:29:20 +0800

    a

--- js/index.js
+++ js/index.js
@@ -1,9 +1,57 @@
 
 window.onload=function(){
-  
-    var mySwiper = new Swiper ('.swiper-container', {
-        direction: 'vertical',
-      	loop: false,
-        
-    }) 
+
+	var mySwiper = new Swiper ('.swiper-container', {
+		direction: 'vertical',
+		loop: false,
+
+	});
+
+    // $("#box1-top").addClass('animated');
+    $("#box1-top img").css("animation","Move1 0.8s ease-in-out forwards");
+    // $('body').oneTime('1s',function(){
+    // 	$("#box1-z1 p").css("display","block");
+    // 	$("#box1-z1 p").addClass('animated tada')
+    // });
+    $(function(){
+    	function aa(){
+    		$("#box1-z1 p").css("display","block");
+    		$("#box1-z1 p").addClass('animated tada')
+    	}
+    	setInterval(aa,1000); 
+    });
+
+    $(function(){
+    	function bb(){
+    		$("#box1-m2 img").css("display","block");
+    		$("#box1-m2 img").addClass('animated slideInLeft')
+    	}
+    	setInterval(bb,1500); 
+    });
+
+    $(function(){
+    	function cc(){
+    		$("#box1-z2 p").css("display","block");
+    		$("#box1-z2 p").addClass('animated slideInRight')
+    	}
+    	setInterval(cc,1500); 
+    });
+
+    $(function(){
+    	function cc(){
+    		$("#box1-z3 p").slideDown("2s",function(){
+    		})
+    		// $("#box1-z3 p").addClass('animated slideInRight')
+    	}
+    	setInterval(cc,2000); 
+    });
+
+    $(function(){
+    	function dd(){
+    		$("#box1-z4 p").show();
+    		$("#box1-z4 p").addClass('animated rollIn')
+    	}
+    	setInterval(dd,2100); 
+    });
+
 }
